9ec279c09f363b17ca74798833338ec6cfad3b35
SantiOAdesign <santiagoosorio2006@gmail.com>
2026-09-10 14:53:36 -0500

    Fix indentation and formatting in sentiment handling

--- app.py
+++ app.py
@@ -36,17 +36,17 @@ with st.expander('Analizar texto'):
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
           with open('Happyjson') as source:
-animation=json. load(source)
-st. lottie(animation,width =350)
+            animation=json. load(source)
+            st. lottie(animation,width =350)
 
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
-with open('Sad Emoji.json') as source:
-animation=json. load(source)
-st. lottie(animation,width =350)
+          with open('Sad Emoji.json') as source:
+            animation=json. load(source)
+            st. lottie(animation,width =350)
 
         else:
             st.write( 'Es un sentimiento Neutral 😐')
           with open('Neutral face.json') as source:
-animation=json. load(source)
-st. lottie(animation,width =350)
+            animation=json. load(source)
+            st. lottie(animation,width =350)
